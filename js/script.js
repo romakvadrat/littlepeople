83acba94558dc29bb7576cb02038f455deb368b6
romakvadrat <romakvadrat@gmail.com>
2020-03-18 20:37:37 +0200

    littlepeople

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener('DOMContentLoaded', function() {
+  // get the form elements defined in your form HTML above
 
-    // get the form elements defined in your form HTML above
-    
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var status = document.getElementById("my-form-status");
+  var form = document.getElementById('my-form');
+  var button = document.getElementById('my-form-button');
+  var status = document.getElementById('my-form-status');
 
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      button.style = "display: none ";
-      status.innerHTML = "Надіслано!";
-    }
+  // Success and Error functions for after the form is submitted
 
-    function error() {
-      status.innerHTML = "Ой! Виникла проблема.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
+  function success() {
+    form.reset();
+    button.style = 'display: none ';
+    status.innerHTML = 'Надіслано!';
   }
+
+  function error() {
+    status.innerHTML = 'Ой! Виникла проблема.';
+  }
+
+  // handle the form submission event
+
+  form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
